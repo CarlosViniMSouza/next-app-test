bc4ea73aa73deb8cd16e8d5ec76cf98732a4c522
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2023-09-25 11:35:34 -0300

    next sub-theme: Getting Started with React

---
+++ autogit.sh
@@ -0,0 +1,8 @@
+git add .
+git commit -m "next sub-theme: Getting Started with React"
+git branch -M main
+git remote add origin https://github.com/CarlosViniMSouza/next-app-test.git
+git push -u origin main
+git status
+
+# continue in: https://nextjs.org/learn/foundations/from-javascript-to-react/getting-started-with-react
